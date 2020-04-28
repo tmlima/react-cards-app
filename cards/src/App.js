68f87e53fc3d51bb47faf73e35d682717d4b2469
tmlima <thiago.machado.lima@gmail.com>
2020-04-27 23:37:37 -0300

    add user form. get data from github not working

--- cards/src/App.js
+++ cards/src/App.js
@@ -1,18 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-    <div>
-      <div className="header">The GitHub Cards App</div>
-      <CardList />
-    </div>
-    );
-  }
-}
-
+import axios from 'axios'
 
 const testData = [
   {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
@@ -22,23 +11,69 @@ const testData = [
 
 const CardList = (props) => (
 <div>
-  {testData.map(profile => <Card {...profile}/>)}
+  {props.profiles.map(profile => <Card {...profile}/>)}
 </div>
 );
 
 class Card extends React.Component {
-render() {
-  const profile = this.props;
-  return (
-    <div className="github-profile">
-      <img src={profile.avatar_url} />
-      <div className="info">
-        <div className="name">{profile.name}</div>
-        <div className="company">{profile.company}</div>
+  render() {
+    const profile = this.props;
+    return (
+      <div className="github-profile">
+        <img src={profile.avatar_url} />
+        <div className="info">
+          <div className="name">{profile.name}</div>
+          <div className="company">{profile.company}</div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+class AddUserForm extends React.Component {
+  state = {username: ''};
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    const user = await axios.get(`https://api.github.users/${this.state.username}`);
+    this.props.onSubmit(user.data);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          value={this.state.username}
+          onChange={event => this.setState({username: event.target.value })}
+          placeholder="Github username"/>
+          <button>Add card</button>
+      </form>
+    )
+  }
+}
+
+class App extends Component {
+
+  state = {
+    profiles: testData,
+  };
+
+  addNewProfile = (profile) => {
+    this.setState(prevState => ({
+      profiles: [...prevState.profiles, profile]
+    }))
+  }
+
+  render() {
+    return (
+    <div>
+      <div className="header">The GitHub Cards App</div>
+      <AddUserForm onSubmit={this.addNewProfile}/>
+      <CardList profiles={this.state.profiles}/>
+    </div>
+    );
+  }
 }
 
 export default App;
